8713488a9734d7cb62bb79a6d1c50788d4ab1d37
Yash Deep <143354431+yashdeep370@users.noreply.github.com>
2025-06-17 12:50:28 +0530

    Delete Array/create array directory

--- Array/create array/main.java
+++
@@ -1,9 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        int [] arr = new int[3];
-        arr[0] = 1;
-        arr[1] = 2;
-        arr[2] = 3;
-        System.out.println("Array elements:" + arr[0] + ", " + arr[1] + ", " + arr[2]);
-    }
-}
